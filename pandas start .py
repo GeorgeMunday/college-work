bd79d54eefb3882b7568db3d1f610c53c790b2d4
George Munday <102688916+GeorgeMunday@users.noreply.github.com>
2024-12-12 09:56:30 +0000

    update

--- pandas start .py
+++ pandas start .py
@@ -11,5 +11,23 @@ print(df)
 
 
 
+#shows pandas version you are useing
+print(pd.__version__)
 
+a = [1, 7, 2]
+
+myvar = pd.Series(a)
+
+print(myvar)
+
+
+
+dat = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(dat, index = ["day1", "day2", "day3"])
+
+print(df) 
 
