f7d611a3d4b90afedcdfec3a857b15b152bf4d85
George Munday <102688916+GeorgeMunday@users.noreply.github.com>
2024-12-12 09:42:14 +0000

    update

---
+++ pandas start .py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Creating a DataFrame
+data = {
+'Name': ['John', 'Emma', 'Michael', 'Sophia'],
+'Height': [5.5, 6.0, 5.8, 5.3],
+'Qualification': ['BSc', 'BBA', 'MBA', 'BSc']
+}
+df = pd.DataFrame(data)
+print(df)
+
+
+
+
+
